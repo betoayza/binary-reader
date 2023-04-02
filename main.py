8b065b318f32d91d43e77873345c9bc8eef564e7
betoayza <aaayza@gmail.com>
2023-04-02 12:37:44 -0300

    Finished

---
+++ main.py
@@ -0,0 +1,10 @@
+binary_code = input("Enter binary code: ")
+
+bytes_list = binary_code.split()
+
+ascii_text = ""
+for byte in bytes_list:
+    decimal = int(byte, 2)
+    ascii_text += chr(decimal)
+
+print(ascii_text)
